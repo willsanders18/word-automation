4a92003709eb09cdc9f5c360cab740d5c98e42bd
Will <Willsandersnpt@gmail.com>
2025-02-11 11:42:07 -0500

    Progress
    progress

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from docx import Document
+from docx.shared import Pt
+from docx.shared import Length
+import os
+
+document = Document()
+
+style = document.styles['Normal']
+font = style.font
+
+font.name = 'Times New Roman'
+font.size = Pt(12)
+
+paragraph = document.add_paragraph()
+paragraph_format = paragraph.paragraph_format
+paragraph_format
+
+
 
